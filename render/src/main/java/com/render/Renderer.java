15f58219cf7e58c277589da4bb55a5f669b98ba3
Vlad <55758407+svp8@users.noreply.github.com>
2024-01-26 12:23:23 +0300

    Update Renderer.java

--- render/src/main/java/com/render/Renderer.java
+++ render/src/main/java/com/render/Renderer.java
@@ -6,7 +6,6 @@ import com.render.model.RenderedImage;
 import java.awt.image.BufferedImage;
 
 public class Renderer {
-    private final static double[] initialBounds = {-13884991, 2870341, -7455066, 6338219};
 
     public static RenderedImage createImage(int width, int height, BBox bbox) {
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
